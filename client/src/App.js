dad81c0185c1ccaca2ea5e1bc653baab20513831
Heechan Yang <heechy06@naver.com>
2022-08-22 18:25:46 +0900

    8.22 checkpoint 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,45 @@
-import './App.css';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import React from 'react'
-import { StyleSheet, View} from "react-native-web"
+import "./App.css";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import React from "react";
+import { StyleSheet, View } from "react-native-web";
 
 function App() {
-  const [value, setValue] = React.useState('');
-  const [translated, setTranslated] = React.useState('')
+  const [value, setValue] = React.useState("");
+  const [translated, setTranslated] = React.useState("");
 
   const handleChange = (event) => {
     setValue(event.target.value);
   };
 
   const buttonClick = (event) => {
-    console.log('button clicked!')
-    setTranslated('loading...')
+    console.log("button clicked!");
+    setTranslated("loading...");
 
-    fetch('/translate', {
-      method: 'POST',
+    fetch("/translate", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         text: value,
       }),
-    }).then(
-        res => res.text()
-      ).then(
-        translated => {
-          setTranslated(translated)
-          console.log(translated)
-        }
-    )
+    })
+      .then((res) => res.text())
+      .then((translated) => {
+        setTranslated(translated);
+        console.log(translated);
+      });
 
-    alert('작성 완료')
+    alert("작성 완료");
   };
 
   const clearClick = (event) => {
-    console.log('clear clicked!')
+    console.log("clear clicked!");
 
-    setValue('')
-    setTranslated('')
-  }
+    setValue("");
+    setTranslated("");
+  };
 
   const styles = StyleSheet.create({
     container: {
@@ -53,66 +51,78 @@ function App() {
   return (
     <div>
       <div>
-        <div 
+        <div
           className="app-header"
           style={{
-            textAlign: 'center',
-          }}>
-            <h2 className="header">한글 라인 브레이커</h2>
+            textAlign: "center",
+          }}
+        >
+          <h2 className="header">한글 라인 브레이커</h2>
         </div>
       </div>
 
       <div
         style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
 
-        <div
-          syle={{
-            display: 'flex',
-          }}>
-          <TextField
-            id="outlined-multiline-static"
-            label="Texts"
-            multiline
-            rows={15}
-            value={value}
-            onChange={handleChange}
-          />
-        </div>
-        
-        <View style={[styles.containor, {
-          flexDirection: "column"
-        }]}>
-          <Button 
+        <TextField
+          id="outlined-multiline-static"
+          label="Texts"
+          multiline
+          rows={15}
+          value={value}
+          onChange={handleChange}
+          style={{
+            width: "100%",
+          }}
+        />
+
+        <View
+          style={[
+            styles.containor,
+            {
+              flexDirection: "column",
+            },
+          ]}
+        >
+          <Button
             variant="contained"
             onClick={buttonClick}
             style={{
-              margin: '20px',
-            }}>라인 브레이크</Button>
+              margin: "20px",
+            }}
+          >
+            라인 브레이크 
+          </Button>
 
-          <Button 
+          <Button
             variant="contained"
             onClick={clearClick}
             style={{
-              margin: '20px',
-            }}>삭제</Button>
+              margin: "20px",
+            }}
+          >
+            - 삭제 -
+          </Button>
         </View>
-        
-        <div
-          syle={{
-            display: 'flex',
-          }}>
-          <TextField
-            id="outlined-multiline-static"
-            label="Line Breaked Texts"
-            multiline
-            rows={15}
-            value={translated}
-          />
-        </div>
+
+        <TextField
+          id="outlined-multiline-static"
+          // label="Line Breaked Texts"
+          multiline
+          rows={15}
+          defaultValue={translated}
+          inputProps={{
+            readOnly: true,
+          }}
+          style={{
+            width: "100%",
+          }}
+        />
       </div>
     </div>
   );
